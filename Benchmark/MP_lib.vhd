3472748f4f28d4816948a9378d5bf824cec87035
Tris10 <tristent1@hotmail.com>
2019-02-25 12:04:09 -0400

    adding opcode for addresses bigger than 8 bits

--- Benchmark/MP_lib.vhd
+++ Benchmark/MP_lib.vhd
@@ -20,6 +20,8 @@ constant jz  : std_logic_vector(3 downto 0) := "0110";
 constant halt  : std_logic_vector(3 downto 0) := "1111";
 constant readm  : std_logic_vector(3 downto 0) := "0111";
 constant mov5  : std_logic_vector(3 downto 0) := "1000"; -- indirect read
+constant mov6  : std_logic_vector(3 downto 0) := "1001"; -- immediate set 12 bit number hard coded to R15
+
 
 component CPU is
 port (	
